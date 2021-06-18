a7157e9fd94bd329023262f113aea854a680cb06
khouzi <khouzem.boussaid@gmail.com>
2021-06-18 05:33:39 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int j;
+char hex[16]="0123456789abcdef";
+for (j = 0 ; j < 16 ; j++)
+{
+        putchar(hex[j]);
+}
+putchar('\n');
+return (0);
+}
