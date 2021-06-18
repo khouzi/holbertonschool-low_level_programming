62abfe602f8e043ed518f16d9fb26269914f6a14
khouzi <khouzem.boussaid@gmail.com>
2021-06-18 05:02:37 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@ int main(void)
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-	if ( ch != 'q' && ch !='e')
+	if (ch != 'q' && ch !='e')
 	putchar(ch);
 }
 putchar('\n');
-return(0);
+return (0);
 }
