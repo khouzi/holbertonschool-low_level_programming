3af17cb6bebd93f476659d42b438686e72eaf167
khouzi <khouzem.boussaid@gmail.com>
2021-06-18 06:10:16 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,13 @@ int main(void)
 int x;
 for (x = 0; x < 10; x++)
 {
-        putchar((x % 10) + '0');
+	putchar((x % 10) + '0');
 if (x == 9)
 {
-        continue;
+	continue;
 }
-        putchar(',');
-        putchar(' ');
+	putchar(',');
+	putchar(' ');
 }
 putchar('\n');
 return (0);
