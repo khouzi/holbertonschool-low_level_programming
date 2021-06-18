b7403ea11b95ef52bab1ce871c6ac121263e257a
khouzi <khouzem.boussaid@gmail.com>
2021-06-18 06:40:58 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-int i,j;
-for (i=0 ; i<9 ; i++)
+int i, j;
+for (i = 0 ; i < 9 ; i++)
 {
-	for (j=1 ; j<=9; j++ )
+	for (j = 1 ; j <= 9; j++ )
 {
 	if (i < j)
 {
-	printf("%d%d ",i,j);
-	if (i!=8 || j!=9 )
+	printf("%d%d", i, j);
+	if (i != 8 || j != 9 )
 {
 	putchar(',');
 	putchar(' ');
