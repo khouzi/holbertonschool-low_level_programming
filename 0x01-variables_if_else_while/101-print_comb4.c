8a04a5d76b434ddb10b087d8bddbcdfb003cabbc
khouzi <khouzem.boussaid@gmail.com>
2021-06-18 06:48:33 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,7 +12,7 @@ for (i = 0 ; i < 9 ; i++)
 	for (j = 1 ; j <= 9; j++)
 {
 	for (k = 1 ; k <= 9; k++)
-	if (i < j && j < k )
+	if (i < j && j < k)
 {
 	printf("%d%d%d", i, j, k);
 	if (i != 7 || j != 8 || k != 9)
