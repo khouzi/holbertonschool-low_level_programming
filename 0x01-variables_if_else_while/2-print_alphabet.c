effb4f3b271cca4d4830941e8ef45df1fd084c5b
khouzi <khouzem.boussaid@gmail.com>
2021-06-18 04:32:55 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 char ch;
-for(ch = 'a' ; ch <= 'z' ; ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 	putchar(ch);
 }
 putchar('\n');
-return(0);
+return (0);
 }
