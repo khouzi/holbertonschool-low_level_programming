32dc3b16b76ae45a01a963a11b267c068b4efe1f
khouzi <khouzem.boussaid@gmail.com>
2021-06-18 06:38:56 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i,j;
+for (i=0 ; i<9 ; i++)
+{
+	for (j=1 ; j<=9; j++ )
+{
+	if (i < j)
+{
+	printf("%d%d ",i,j);
+	if (i!=8 || j!=9 )
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
+}
+printf("\n");
+return (0);
+}
