e8e4e410b4ac1a5b153f75ee88600f518e5474f2
khouzi <khouzem.boussaid@gmail.com>
2021-06-22 10:56:19 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,18 @@
 #include "holberton.h"
-#include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet_x10(void)
 {
-	int i;
+	int count = 0;
+	char letter;
 
-	for (i = 0; i < 10; i++)
-		print_alphabet();
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
 }
