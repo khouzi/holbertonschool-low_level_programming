4470c8a11908009a44e0faccb31527b9a9483fb8
khouzi <khouzem.boussaid@gmail.com>
2021-06-24 10:32:09 -0700

    Task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,17 @@
 #include "holberton.h"
-
 /**
- * main - check the code for Holberton School students.
+ * print_most_numbers -  prints the numbers, from 0 to 9 except 2 and 4
  *
- * Return: Always 0.
+ * Return: 0
  */
-
 void print_most_numbers(void)
 {
 	int i;
-	for ( i = 0; i <= 9; i++)
+
+	for (i = 0; i <= 9; i++)
 	{
-		if (i == 2 || i == 4)
-			i++;
-		else
-			_putchar(i);
+		if (i != 2 && i != 4)
+			_putchar ((i % 10) + '0');
 	}
-	_putchar('\n');
+	_putchar ('\n');
 }
