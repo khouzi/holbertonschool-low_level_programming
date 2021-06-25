2d1dfe56af647bc4fd926588e7e0670fcc9a4ef2
khouzi <khouzem.boussaid@gmail.com>
2021-06-25 01:00:45 -0700

    Task11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void factor_prime(long);
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	factor_prime(1231952);
+	return (0);
+}
+
+/**
+ * factor_prime - prints the prime factors of a number
+ * @n: number
+ */
+
+void factor_prime(long n)
+{
+	int i, x, count, res;
+
+	for (i = n - 1; i > 0; i--)
+	{
+		count = 0;
+		x = 1;
+		while (x <= i)
+		{
+			if (i % x == 0 && i > 1)
+				count++;
+			x++;
+		}
+		if (count <= 2)
+		{
+			if (n % i == 0)
+			{
+				res = i;
+				printf("%d ", res);
+				break;
+			}
+		}
+	}
+	printf("\n");
+}
