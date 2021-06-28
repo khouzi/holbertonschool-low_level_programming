2097bb1ca7581223ff6d23cc819df4f42d5cfa00
khouzi <khouzem.boussaid@gmail.com>
2021-06-28 02:23:56 -0700

    Task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * main :Write a function that takes a pointer to an int as parameter and update * the value it points to to 98.
  *
- *@c : *n a pointer
+ * @c : *n a pointer
  */
 
 void reset_to_98(int *n)
