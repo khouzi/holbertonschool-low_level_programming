bad18e6ecf1b5aeaec58059649d9d560cdde4707
khouzi <khouzem.boussaid@gmail.com>
2021-06-28 05:54:31 -0700

    Task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _puts - Write a function that prints a string,
+ *         followed by a new line, to stdout.followed by a new line,
+ *         to stdout.
+ * @str: pointer to a character.
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+		*str++;
+	_putchar('\n');
+}
