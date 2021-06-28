8faf184bffbe4bea9a41f953d0563df6028eea36
khouzi <khouzem.boussaid@gmail.com>
2021-06-28 05:43:29 -0700

    Task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,30 @@
 #include "holberton.h"
-
+int _strlen(char *s);
 /**
- * print_rev - Prints a string in reverse.
+ * print_rev - function that prints a string, in reverse.
  * @s: The string to be printed.
  */
-
 void print_rev(char *s)
 {
-	int L = 0;
-	char T
-	while (*s++)
-		L++;
-	for (*s = L - 1; *s >= L / 2; *s --)
-	{
-		T = *s;
-		*s = *s --;
-		*s = T;
-	}
-}
+	int c;
+	int len = _strlen(s);
+
+	for (c = len - 1; c >= 0; c--)
+		_putchar(s[c]);
 	_putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
 }
