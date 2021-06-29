5712e0e1187ef5342deff8dd89a75b51c15f2881
khouzi <khouzem.boussaid@gmail.com>
2021-06-29 01:19:55 -0700

    Task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,29 @@
 #include "holberton.h"
-
+int _strlen(char *s);
 /**
- * _puts - Write a function that prints a string,
- *         followed by a new line, to stdout.followed by a new line,
- *         to stdout.
- * @str: pointer to a character.
+ * puts2 - prints a string, in reverse, followed by a new line
+ * @str: pointer to string
  */
 void puts2(char *str)
 {
-	while (*str)
-	{
-		_putchar(*str++);
-		str++;
-	}
+	int c;
+	int len = _strlen(str);
+
+	for (c = 0; c < len; c += 2)
+		_putchar(str[c]);
 	_putchar('\n');
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
 }
