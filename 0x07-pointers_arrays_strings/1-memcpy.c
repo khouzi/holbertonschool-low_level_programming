4de2b59ab2cd2cf189e765845f61b066e0375f4d
khouzi <khouzem.boussaid@gmail.com>
2021-07-05 02:26:32 -0700

    Task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * memcpy - writes a function that copies memory area
+ *
+ * @dest: A pointer to the memory area to be filled.
+ * @src: A pointer to the memory area to be filled.
+ * @n: The number of bytes to be filled.
+ *
+ * Return: A pointer to the filled memory area @dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	for (dest; *dest <= n; dest++)
+	{
+		*dest = *src;
+		*src++;
+	}
+	return (dest);
+}
