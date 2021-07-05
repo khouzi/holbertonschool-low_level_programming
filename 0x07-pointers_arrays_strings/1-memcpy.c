1a3b0b4ecbb855939b003e91f109e748e5b66b4a
khouzi <khouzem.boussaid@gmail.com>
2021-07-05 04:27:54 -0700

    Task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	for (dest; *dest <= n; dest++)
+	int i;
+
+	for (i = 0; i <= n; i++)
 	{
-		*dest = *src;
-		*src++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
