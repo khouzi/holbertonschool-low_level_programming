5f3d303a1dc998d339dc5a847d91d17c3afb3506
khouzi <khouzem.boussaid@gmail.com>
2021-07-06 18:17:11 -0700

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,15 +3,13 @@
 /**
  * _print_rev_recursion - write a function that prints
  *                        a strings in reverse.
- * 
+ *
  * @s : pointer to a character.
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\n')
-		_putchar ('\n');
-	else if (*s != '\0')
+	if (*s != '\0')
 	{
 		s++;
 		_print_rev_recursion(s);
