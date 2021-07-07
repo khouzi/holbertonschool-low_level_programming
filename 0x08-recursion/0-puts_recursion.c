bc8c70782bfa9032a5cd901a19263753be3a2f04
khouzi <khouzem.boussaid@gmail.com>
2021-07-06 17:55:23 -0700

    Task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion - write a function that prints a string followed by a new line
+ * _puts_recursion - write a function that prints a string,
+ *                   followed by a new line.
  *
  * @s : piinter to a string.
  */
@@ -11,5 +12,8 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 		_putchar('\n');
 	else
+	{
 		_putchar(*s);
+	}
+	_puts_recursion(*s);
 }
