eb42e3e478a50a32a4bbf42583300c1506d55b4a
khouzi <khouzem.boussaid@gmail.com>
2021-07-06 19:13:12 -0700

    task4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,8 +14,8 @@ int _pow_recursion(int x, int y)
 {
 	if (y < x)
 		return (-1);
-	else (y = 1)
+	else (y == 1)
 		return (1);
 	else
-		return (x * _pow_recursion(y - 1));
+		return (x * _pow_recursion(x,y - 1));
 }
