a9c7890ed7f306897258f6c836ba136ec56426ad
khouzi <khouzem.boussaid@gmail.com>
2021-07-06 20:16:22 -0700

    Task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,29 @@
 #include "holberton.h"
 
+#include "holberton.h"
+
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+
+/**
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @d: The divisor.
+ *
+ * Return: If the number is divisible - 0.
+ *         If the number is not divisible - 1.
+ */
+int is_divisible(int n, int d)
+{
+	if (n % d == 0)
+		return (0);
+
+	if (d == n / 2)
+		return (1);
+
+	return (is_divisible(n, d + 1));
+}
+
 /**
  * is_prime_number - Checks if a number is prime.
  * @n: The number to be checked.
@@ -7,16 +31,15 @@
  * Return: If the integer is not prime - 0.
  *         If the number is prime - 1.
  */
-
 int is_prime_number(int n)
 {
+	int k = 2;
+
 	if (n <= 1)
 		return (0);
-	if (n == 2)
+
+	if (n >= 2 && n <= 3)
 		return (1);
-	if (n % 2 == 0)
-		return (0);
-	if (2 == n / 2)
-		return (1);
-	return (is_prime_number(n));
+
+	return (is_divisible(n, k));
 }
