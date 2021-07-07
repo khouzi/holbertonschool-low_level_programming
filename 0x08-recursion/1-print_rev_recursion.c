894f68c03091b6f84aa3961f9b52939dc199b86b
khouzi <khouzem.boussaid@gmail.com>
2021-07-06 18:55:05 -0700

    Task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,15 +9,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
-		s++;
-		_print_rev_recursion(s);
-	}
-	else
-	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		s--;
-		_print_rev_recursion(s);
 	}
 }
