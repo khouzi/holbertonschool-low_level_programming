ed3baeed68b79241450d9cb65578949f2a15bc08
khouzi <62845331+khouzi@users.noreply.github.com>
2021-07-09 04:54:56 -0700

    Task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 /**
- * main - entry point for program
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
- * @argc: count of args present
- * @argv: array of char * pointing to args
- *
- * Return: always 0 (success)
+ * Return: 0 if no errors, else 1
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int a = 0, i, j;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		for (j = 0; argv[i][j]; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (isdigit(argv[i][j]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
 		}
-	sum += atoi(argv[i]);
 	}
-	printf("%d\n", sum);
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
 	return (0);
 }
