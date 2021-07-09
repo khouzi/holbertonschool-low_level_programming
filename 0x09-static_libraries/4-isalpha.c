ab2d5f9d26dc174eb2c03935cd6084b17eaaab36
khouzi <62845331+khouzi@users.noreply.github.com>
2021-07-09 15:32:30 -0700

    Create 4-isalpha.c

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isalpha(int c)- checks for lowercase or uppercase
+ * @c: The character to check
+ *
+ * Return: 1 (Success)
+ */
+int _isalpha(int c)
+{
+	if ((c < 'z' && c > 'a') || (c < 'Z' && c > 'A'))
+		return (1);
+		else
+			return (0);
+}
