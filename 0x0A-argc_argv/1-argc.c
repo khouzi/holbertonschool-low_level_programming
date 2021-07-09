ed1c13e0399a06ea084e7a4244fca0258bb329dd
khouzi <khouzem.boussaid@gmail.com>
2021-07-08 17:56:47 -0700

    Task1

---
+++ 0x0A-argc_argv/1-argc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all arguments it receives.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
