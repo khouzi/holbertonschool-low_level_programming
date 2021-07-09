8932ef73c3b592836474f7f225e4cc021cbf15f9
khouzi <62845331+khouzi@users.noreply.github.com>
2021-07-09 15:31:40 -0700

    Create 3-islower.c

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _islower(int c)- checks for lowercase character
+ * @c: The character to check
+ *
+ * Return: 1 (Success)
+ */
+int _islower(int c)
+{
+	if (c < 'z' && c > 'a')
+		return (1);
+		else
+			return (0);
+}
