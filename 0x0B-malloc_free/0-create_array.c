cb6ba35f250a41c205487f971d605c04e134b0e2
khouzi <khouzem.boussaid@gmail.com>
2021-07-12 20:08:53 -0700

    Task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 char *create_array(unsigned int size, char c)
 {
 	char *T;
 
-	if (size == NULL)
-		return (0);
+	if (size == 0)
+		return (NULL);
 	T = malloc(size);
 	T[0] = c;
 	return (T);
