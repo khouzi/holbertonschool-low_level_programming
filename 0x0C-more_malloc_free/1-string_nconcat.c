3c7148f45dcdd282e171b474c7430744f34d2a88
khouzi <62845331+khouzi@users.noreply.github.com>
2021-07-15 07:53:30 -0700

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,44 @@
 #include "holberton.h"
-#include <stdlib.h>
+
 /**
- * string_nconcat - concatenates two strings, up to n bytes of s2
+ * string_nconcat - concatenate two strings
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the maximum number of bytes to use from s2
  *
- * @s1: string 1 to concatenate with string 2
- * @s2: string 2 to concatenate with string 1
- * @n: number of bytes to concatenate of s2
- *
- * Return: pointer to new string, or NULL on failure
+ * Return: If memory allocation fails, return NULL. Otherwise, return a pointer
+ * to the first character of the new string.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, k = 0;
-	char *T, *Tstart;
+	char *cat;
+	unsigned int s1_len = 0;
+	unsigned int s2_len = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (*(s1 + i))
-		i++;
-	while (*(s2 + j))
-		j++;
-	if (n >= j)
+	if (s1)
 	{
-		n = j;
-		T = malloc(sizeof(char) * (i + n + 1));
+		while (s1[s1_len])
+			++s1_len;
 	}
-	else
-		T = malloc(sizeof(char) * (i + j + 1));
-	if (T == NULL)
+
+	if (s2)
+	{
+		while (s2_len < n && s2[s2_len])
+			++s2_len;
+	}
+
+	cat = malloc(sizeof(char) * s1_len + s2_len + 1);
+	if (!cat)
 		return (NULL);
-	Tstart = T;
-	while (*s1)
-		*T++ = *s1++;
-	while (k < n)
-	{
-		*T++ = *s2++;
-		k++;
-	}
-	*T = '\0';
-	return (Tstart - i - j);
+
+	for (n = 0; n < s1_len; ++n, ++cat)
+		*cat = s1[n];
+
+	for (n = 0; n < s2_len; ++n, ++cat)
+		*cat = s2[n];
+
+	*cat = '\0';
+
+	return (cat - s1_len - s2_len);
 }
+{"mode":"full","isActive":false}
