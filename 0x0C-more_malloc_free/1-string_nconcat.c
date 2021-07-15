e542f0c7afbbd1324fce515f039377cb24ee9152
khouzi <khouzem.boussaid@gmail.com>
2021-07-15 07:09:42 -0700

    Task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,30 +11,31 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1Size = 0, s2Size = 0, i = 0;
-	char *conc, *concStart;
+        unsigned int i = 0, j = 0, k = 0;
+        char *T, *Tstart;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (*(s1 + s1Size))
-		s1Size++;
-	while (*(s2 + s2Size))
-		s2Size++;
-	if (n > s2Size)
-		n = s2Size;
-	conc = malloc(sizeof(char) * (s1Size + s2Size + 1));
-	if (conc == NULL)
-		return (NULL);
-	concStart = conc;
-	while (*s1)
-		*conc++ = *s1++;
-	while (i < n)
-	{
-		*conc++ = *s2++;
-		i++;
-	}
-	*conc = '\0';
-	return (concStart);
+        if (s1 == NULL)
+                s1 = "";
+        if (s2 == NULL)
+                s2 = "";
+        while (*(s1 + i))
+                i++;
+        while (*(s2 + j))
+                j++;
+        if (n > j)
+                n = j;
+        T = malloc(sizeof(char) * (i + j + 1));
+        if (T == NULL)
+                return (NULL);
+        Tstart = T;
+        while (*s1)
+                *T++ = *s1++;
+        while (k < n)
+        {
+                *T++ = *s2++;
+                k++;
+        }
+        *T = '\0';
+
+        return (Tstart);
 }
