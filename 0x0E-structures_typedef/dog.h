076c98ce83941f35e1d03bbd62457e676c2e56f4
khouzi <khouzem.boussaid@gmail.com>
2021-07-19 05:11:09 -0700

    Task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - dog identity
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner:owner of the dog
+ *
+ * Description: gives a name age and ownership to a dog
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
