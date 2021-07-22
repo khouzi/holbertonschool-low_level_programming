3cfaad17d5209e9024c90880a0d7c996d20e4f06
khouzi <khouzem.boussaid@gmail.com>
2021-07-22 12:32:26 -0700

    Task4

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,15 +1,14 @@
-global main
+	global	main
 
-section .text
-
+	section	.text
 main:	mov	rax, 1
 	mov	rdi, 1
-	mov	rsi, msg
-	rds,	17
+	mov	rsi, message
+	mov	rdx, 17
 	syscall
 	mov	rax, 60
 	xor	rdi, rdi
 	syscall
 
-	section .data
-msg:db		"Hello, Holberton", 10
+	section	.data
+message:db	"Hello, Holberton", 10
