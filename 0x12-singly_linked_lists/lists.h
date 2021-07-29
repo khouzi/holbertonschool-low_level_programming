d63e7414c611e4efb4e96228ece777570b828281
khouzi <62845331+khouzi@users.noreply.github.com>
2021-07-29 04:04:52 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef LIST_H
+#define LIST_H
 
 /**
  * struct list_s - singly linked list
@@ -16,10 +16,12 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(const char *s);
 
 #endif
