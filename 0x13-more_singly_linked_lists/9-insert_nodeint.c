f5d290c097e7f56b1267ac8d5568b656392b8ca0
khouzi <62845331+khouzi@users.noreply.github.com>
2021-08-03 13:37:01 -0700

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node = malloc(sizeof(listint_t));
 		if (!new_node)
 			return (NULL);
-		new_node->n = n; /* set data */
+		new_node->n = n;
 		new_node->next = temp_h;
 		*head = new_node;
 		return (new_node);
@@ -42,4 +42,3 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	return (NULL);
 }
-© 
