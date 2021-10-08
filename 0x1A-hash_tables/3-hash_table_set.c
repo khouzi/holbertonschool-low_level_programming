98538afd4a6ddfde4fa9809ed069cb073a5a0ad6
khouzi <khouzem.boussaid@gmail.com>
2021-10-08 16:25:43 +0200

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	while (tmp != NULL)
 	{
-		if (strcmp(tmp->key, key) == 0)
+		if(strcmp(tmp->key, key) == 0)
 			{
 				free(tmp->value);
 				tmp->value = strdup(value);
