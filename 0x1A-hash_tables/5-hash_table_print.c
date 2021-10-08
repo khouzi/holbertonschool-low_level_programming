682eb0886d20b7d69645ddb5d5c779d0d687e710
khouzi <khouzem.boussaid@gmail.com>
2021-10-08 16:59:23 +0200

    5-hash_table_print

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,11 +6,23 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int idx = 0;
+	unsigned long int i = 0;
 	hash_node_t *node;
 	char *comma = "";
 
-    if (ht == NULL || !ht->array)
-        return;
-    putchar('{');
+	if (ht == NULL || !ht->array)
+		return;
+	putchar('{');
+	while (i < ht->size)
+	{
+		node = ((ht->array)[i]);
+		while (node)
+		{
+			printf("%s'%s': '%s'", comma, node->key, node->value);
+			comma = ", ";
+			node = node->next;
+		}
+		i++;
+	}
+	putchar("}");
 }
